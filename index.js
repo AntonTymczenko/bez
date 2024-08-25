7c0b72db9e032886f1ba20d877deaa58fe460b48
Anton Tymczenko <a.tymczenko@gmail.com>
2024-08-25 15:59:55 +0200

    feature: express server and index.html

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const path = require('path');
+const app = express();
+const APP_NAME = 'bez';
+let port = 8080;
+
+if (process.argv.length > 2) {
+  const p = parseInt(process.argv[2], 10);
+  if (isNaN(p)) {
+    console.error(`Invalid port number. Using default port ${port}.`);
+  } else {
+    port = p;
+  }
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App ${APP_NAME} listening on port ${port}`);
 })
